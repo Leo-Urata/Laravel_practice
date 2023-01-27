676192847f7331db05ba2b492a9180c7a669ee60
Leo-Urata <s2022054@stu.musashino-u.ac.jp>
2023-01-27 14:01:05 +0900

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Folder;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(int $id)
+    {
+        // 全てのフォルダを取得する
+        $folders = Folder::all();
+
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+
+        // 選べれたフォルダに紐づくタスクを取得する
+        $tasks = $current_folder->tasks()->get();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
+        ]);
+    }
+
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id
+        ]);
+    }
+}
