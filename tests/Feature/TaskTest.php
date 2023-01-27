eab3a596ce647286f6ee19107d68ae9f6f118e6f
Leo-Urata <s2022054@stu.musashino-u.ac.jp>
2023-01-27 15:58:04 +0900

    TaskTest.php を修正しました。

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -32,11 +32,11 @@ class TaskTest extends TestCase
     {
         $response = $this->post('/folders/1/tasks/create', [
             'title' => 'Sample task',
-            'due_date' => 123,
+            'due_date' => 123, // 不正なデータ（数値）
         ]);
 
         $response->assertSessionHasErrors([
-            'due_date' => '期限日には日付を入力してください。',
+            'due_date' => '期限日 には日付を入力してください。',
         ]);
     }
 
@@ -46,13 +46,13 @@ class TaskTest extends TestCase
      */
     public function due_date_should_not_be_past()
     {
-        $response = $this -> post('/folders/1/tasks/create', [
+        $response = $this->post('/folders/1/tasks/create', [
             'title' => 'Sample task',
-            'due_date' => Carbon::yesterday()->format('Y/m/d'),
+            'due_date' => Carbon::yesterday()->format('Y/m/d'), // 不正なデータ（昨日の日付）
         ]);
 
         $response->assertSessionHasErrors([
-            'due_date' => '期限日には今日以降の日付を入力してください。',
+            'due_date' => '期限日 には今日以降の日付を入力してください。',
         ]);
     }
 }
