16fcccc8783e18a2828f148226987fa11811964d
Leo-Urata <s2022054@stu.musashino-u.ac.jp>
2023-01-27 15:52:44 +0900

    テストケースを記述しました。

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,21 +2,57 @@
 
 namespace Tests\Feature;
 
+use App\Http\Requests\CreateTask;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    // テストケースごとにデータベースをリフレッシュしてマイグレーションを再実行する
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /**
+     * 各テストメソッドの実行前に呼ばれる
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        // テストケース実行前にフォルダデータを作成する
+        $this->seed('FoldersTableSeeder');
+    }
+
+    /**
+     * 期限日が日付ではない場合はバリデーションエラー
+     * @test
+     */
+    public function due_date_should_be_date()
+    {
+        $response = $this->post('/folders/1/tasks/create', [
+            'title' => 'Sample task',
+            'due_date' => 123,
+        ]);
+
+        $response->assertSessionHasErrors([
+            'due_date' => '期限日には日付を入力してください。',
+        ]);
+    }
+
+    /**
+     * 期限日が過去日付の場合はバリデーションエラー
+     * @test
+     */
+    public function due_date_should_not_be_past()
+    {
+        $response = $this -> post('/folders/1/tasks/create', [
+            'title' => 'Sample task',
+            'due_date' => Carbon::yesterday()->format('Y/m/d'),
+        ]);
+
+        $response->assertSessionHasErrors([
+            'due_date' => '期限日には今日以降の日付を入力してください。',
+        ]);
     }
 }
