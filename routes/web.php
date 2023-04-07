6b1c6fa212cc5845fb95064bfc7d861e5237bbc2
Leo-Urata <s2022054@stu.musashino-u.ac.jp>
2023-04-07 09:56:30 +0900

    タスク編集機能のルーティングを設定しました。

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@ Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
 
 Route::get('/folders/create', [FolderController::class ,'showCreateForm'])->name('folders.create');
 Route::post('/folders/create', [FolderController::class ,'create']);
+
+Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
